aa5c0cbdc94769f2bfa5b6b001842dbfbbca9b35
rhofp <rhodfra@gmail.com>
2020-03-19 00:34:59 -0600

    Added new Controller for api methods to retrieve user answers

--- dev/routes/api.php
+++ dev/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::apiResource('cuestionario','api\CuestionarioController');
+Route::apiResource('respuestas','api\RespuestasUsuarioController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
